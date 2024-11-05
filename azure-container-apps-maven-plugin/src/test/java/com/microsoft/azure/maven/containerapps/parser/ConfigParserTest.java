ca2a361c523ad081a02f161dcef027826967c884
Ruoyu Wang <ruoyuwang@microsoft.com>
2024-11-05 17:01:22 +0800

    fix pom for tests

--- azure-container-apps-maven-plugin/src/test/java/com/microsoft/azure/maven/containerapps/parser/ConfigParserTest.java
+++ azure-container-apps-maven-plugin/src/test/java/com/microsoft/azure/maven/containerapps/parser/ConfigParserTest.java
@@ -128,7 +128,8 @@ public class ConfigParserTest {
     }
 
     @Test
-    public void testGetContainerAppConfigCode() {
+    public void
+    testGetContainerAppConfigCode() {
         AppContainerMavenConfig containerConfig = new AppContainerMavenConfig();
         containerConfig.setDirectory(".");
         containerConfig.setEnvironment(Collections.emptyList());
